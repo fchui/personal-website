e59ce43ba43c75ed457a25055649e0753bef8a8e
Francis C <fchui@sfu.ca>
2023-08-24 15:41:48 -0700

    Removed unneccessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { Routes, Route, Link, Outlet, NavLink } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Routes>
