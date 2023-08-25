65037dccf740dad393de36852a251e7f66a291ca
Francis C <fchui@sfu.ca>
2023-08-25 03:47:52 -0700

    Nonfunctional header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,104 @@
 import { useState } from 'react'
 import { Routes, Route, Link, Outlet, NavLink } from 'react-router-dom'
-import { MantineProvider, Text} from '@mantine/core'
+import { MantineProvider, 
+  Text,   
+  createStyles,
+  Header,
+  Container,
+  Group,
+  Burger,
+  Paper,
+  Transition,
+  rem,
+} from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks';
 
 import './App.css'
 import React from 'react'
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '100%',
+  },
+
+  links: {
+    [theme.fn.smallerThan('xs')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('xs')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    display: 'block',
+    lineHeight: 1,
+    padding: `${rem(8)} ${rem(12)}`,
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    },
+  },
+
+  linkActive: {
+    '&, &:hover': {
+      backgroundColor: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).background,
+      color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
+    },
+  },
+}));
+
+interface HeaderSimpleProps {
+  links: { link: string; label: string }[];
+}
+
+export function HeaderSimple({ links }: HeaderSimpleProps) {
+  const [opened, { toggle }] = useDisclosure(false);
+  const [active, setActive] = useState(links[0].link);
+  const { classes, cx } = useStyles();
+
+  const items = links.map((link) => (
+    <a
+      key={link.label}
+      href={link.link}
+      className={cx(classes.link, { [classes.linkActive]: active === link.link })}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(link.link);
+      }}
+    >
+      {link.label}
+    </a>
+  ));
+
+  return (
+    <Header height={60} mb={120}>
+      <Container className={classes.header}>
+        <Group spacing={10} className={classes.links}>
+          {items}
+        </Group>
+        <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+      </Container>
+    </Header>
+  );
+}
 
 const App = () => {
   return (
     <>
       <MantineProvider withGlobalStyles withNormalizeCSS>
+      <HeaderSimple links={[{ "link": "/Home", "label": "Home" },{ "link": "/Projects", "label": "Projects" }]}></HeaderSimple>
         <Text>Welcome to Mantine!</Text>
       </MantineProvider>
       <Routes>
