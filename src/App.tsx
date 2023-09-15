fe2f696812112247895c7f561b1f39492757aae6
Francis C <fchui@sfu.ca>
2023-09-14 18:44:49 -0700

    Added header and card Templates

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,11 @@ import { MantineProvider,
   createStyles,
   Header,
   Container,
+  Title,
+  Badge,
+  Card,
+  SimpleGrid,
+  Button,
   Group,
   Burger,
   Paper,
@@ -12,9 +17,31 @@ import { MantineProvider,
   rem,
 } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks';
+import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
 
 import './App.css'
 import React from 'react'
+
+const mockdata = [
+  {
+    title: 'Extreme performance',
+    description:
+      'This dust is actually a powerful poison that will even make a pro wrestler sick, Regice cloaks itself with frigid air of -328 degrees Fahrenheit',
+    icon: IconGauge,
+  },
+  {
+    title: 'Privacy focused',
+    description:
+      'People say it can run at the same speed as lightning striking, Its icy body is so cold, it will not melt even if it is immersed in magma',
+    icon: IconUser,
+  },
+  {
+    title: 'No third parties',
+    description:
+      'They’re popular, but they’re rare. Trainers who show them off recklessly may be targeted by thieves',
+    icon: IconCookie,
+  },
+];
 
 const useStyles = createStyles((theme) => ({
   header: {
@@ -57,6 +84,119 @@ const useStyles = createStyles((theme) => ({
       color: theme.fn.variant({ variant: 'light', color: theme.primaryColor }).color,
     },
   },
+
+  root: {
+    backgroundColor: '#11284b',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundImage:
+      'linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%), url(https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80)',
+    paddingTop: `calc(${theme.spacing.xl} * 3)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 3)`,
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('md')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  image: {
+    [theme.fn.smallerThan('md')]: {
+      display: 'none',
+    },
+  },
+
+  content: {
+    paddingTop: `calc(${theme.spacing.xl} * 2)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 2)`,
+    marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+    [theme.fn.smallerThan('md')]: {
+      marginRight: 0,
+    },
+  },
+
+  title: {
+    color: theme.white,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontWeight: 900,
+    lineHeight: 1.05,
+    maxWidth: rem(500),
+    fontSize: rem(48),
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+      fontSize: rem(34),
+      lineHeight: 1.15,
+    },
+  },
+
+  description: {
+    color: theme.white,
+    opacity: 0.75,
+    maxWidth: rem(500),
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+    },
+  },
+
+  control: {
+    paddingLeft: rem(50),
+    paddingRight: rem(50),
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: rem(22),
+
+    [theme.fn.smallerThan('md')]: {
+      width: '100%',
+    },
+  },
+
+  titleCard: {
+    fontSize: rem(34),
+    fontWeight: 900,
+
+    [theme.fn.smallerThan('sm')]: {
+      fontSize: rem(24),
+    },
+  },
+
+  descriptionCard: {
+    maxWidth: 600,
+    margin: 'auto',
+
+    '&::after': {
+      content: '""',
+      display: 'block',
+      backgroundColor: theme.fn.primaryColor(),
+      width: rem(45),
+      height: rem(2),
+      marginTop: theme.spacing.sm,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+
+  card: {
+    border: `${rem(1)} solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
+  },
+
+  cardTitle: {
+    '&::after': {
+      content: '""',
+      display: 'block',
+      backgroundColor: theme.fn.primaryColor(),
+      width: rem(45),
+      height: rem(2),
+      marginTop: theme.spacing.sm,
+    },
+  },
 }));
 
 interface HeaderSimpleProps {
@@ -90,6 +230,75 @@ export function HeaderSimple({ links }: HeaderSimpleProps) {
   );
 }
 
+export function HeroImageRight() {
+  const { classes } = useStyles();
+  return (
+    <div className={classes.root}>
+      <Container size="lg">
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              A{' '}
+              <Text
+                component="span"
+                inherit
+                variant="gradient"
+                gradient={{ from: 'pink', to: 'yellow' }}
+              >
+                fully featured
+              </Text>{' '}
+              React components library
+            </Title>
+
+            <Text className={classes.description} mt={30}>
+              Build fully functional accessible web applications with ease – Mantine includes more
+              than 100 customizable components and hooks to cover you in any situation
+            </Text>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export function FeaturesCards() {
+  const { classes, theme } = useStyles();
+  const features = mockdata.map((feature) => (
+    <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
+      <feature.icon size={rem(50)} stroke={2} color={theme.fn.primaryColor()} />
+      <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+        {feature.title}
+      </Text>
+      <Text fz="sm" c="dimmed" mt="sm">
+        {feature.description}
+      </Text>
+    </Card>
+  ));
+
+  return (
+    <Container size="lg" py="xl">
+      <Group position="center">
+        <Badge variant="filled" size="lg">
+          Best company ever
+        </Badge>
+      </Group>
+
+      <Title order={2} className={classes.titleCard} ta="center" mt="sm">
+        Integrate effortlessly with any technology stack
+      </Title>
+
+      <Text c="dimmed" className={classes.descriptionCard} ta="center" mt="md">
+        Every once in a while, you’ll see a Golbat that’s missing some fangs. This happens when
+        hunger drives it to try biting a Steel-type Pokémon.
+      </Text>
+
+      <SimpleGrid cols={3} spacing="xl" mt={50} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
+        {features}
+      </SimpleGrid>
+    </Container>
+  );
+}
+
 const App = () => {
   return (
     <>
@@ -110,7 +319,8 @@ const App = () => {
 const Home = () => {
   return (
       <>
-        <h2>Home</h2>
+        <HeroImageRight />
+        <FeaturesCards />
       </>
   );
 };
@@ -126,28 +336,5 @@ const Projects = () => {
 const NoMatch = () => {
   return (<p>There's nothing here: 404!</p>);
 };
-/*
-const Layout = () => {
-  const style = ({ isActive }) => ({
-    fontWeight: isActive? 'bold' : 'normal',
-  });
-  return (
-  <>
-    <h1>React Router</h1>
-    <nav
-     style={{
-      borderBottom: 'solid 1px',
-      paddingBottom: '1rem',
-     }}
-     >
-      <NavLink to="/home" style={style}>Home</NavLink>
-      <NavLink to="/projects" style={style}>Projects</NavLink>
-     </nav>
-    <main style={{padding:'1rem 0'}}>
-      <Outlet />
-    </main>
-  </>
-  );
-}
-*/
+
 export default App
