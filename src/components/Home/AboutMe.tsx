e331e371ea5c74ad4854206e25a37f1c89db5181
Francis C <fchui@sfu.ca>
2023-09-17 16:30:03 -0700

    Fixed spacing with socials

--- src/components/Home/AboutMe.tsx
+++ src/components/Home/AboutMe.tsx
@@ -42,6 +42,7 @@ const useStyles = createStyles((theme) => ({
     },
 
     social: {
+      paddingTop: `calc(${theme.spacing.xl} * 1)`,
       [theme.fn.smallerThan('sm')]: {
         width: 'auto',
         marginLeft: 'auto',
