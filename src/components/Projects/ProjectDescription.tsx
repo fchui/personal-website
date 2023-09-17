81ca6a365cb914bb3337024e4bdee3ebef09a745
Francis C <fchui@sfu.ca>
2023-09-17 14:35:04 -0700

    Basic Spacing between elements

--- src/components/Projects/ProjectDescription.tsx
+++ src/components/Projects/ProjectDescription.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { createStyles, Text, Container, Grid, Skeleton, rem } from '@mantine/core'
 
 const useStyles = createStyles((theme) => ({
+    wrapper: {
+        paddingTop: `calc(${theme.spacing.xl} * 1)`,
+        paddingBottom: `calc(${theme.spacing.xl} * 1)`,
+    },
+
     grid: {
         borderRight: `${rem(2)} solid ${theme.colors.gray[2]}`,
     }
@@ -12,16 +17,18 @@ export function ProjectDescription() {
 
     return (
         <>
-            <Container>
-                <Grid columns={3}>
-                    <Grid.Col className={classes.grid} span={2}>
-                        <Text>Typing in Skeleton</Text>
-                    </Grid.Col>
-                    <Grid.Col span={1}>
-                        <Skeleton height={300} radius="md" animate={false}/>
-                    </Grid.Col>
-                </Grid>
-            </Container>
+            <div className={classes.wrapper}>
+                <Container>
+                    <Grid columns={3}>
+                        <Grid.Col className={classes.grid} span={2}>
+                            <Text>Typing in Skeleton</Text>
+                        </Grid.Col>
+                        <Grid.Col span={1}>
+                            <Skeleton height={300} radius="md" animate={false}/>
+                        </Grid.Col>
+                    </Grid>
+                </Container>
+            </div>
         </>
     )
   }
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 
-import { Title, Container} from '@mantine/core'
+import { createStyles, Title, Container} from '@mantine/core'
 import { PreviewCarousel, ImageCarousel, ProjectDescription } from '../components/index';
 
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+      paddingTop: `calc(${theme.spacing.xl} * 1)`,
+      paddingBottom: `calc(${theme.spacing.xl} * 1)`,
+  }
+}));
+
 export const Projects = () => {
+  const { classes, theme } = useStyles();
+
   return (
     <>
       <PreviewCarousel />
       <Container>
-        <Title align="left">Project Title</Title>
+        <Title className={classes.wrapper} align="left">Project Title</Title>
         <ImageCarousel />
         <ProjectDescription />
       </Container>
