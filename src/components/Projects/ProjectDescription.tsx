cea9a45887366873fccbca349a49b8118a308c87
Francis C <fchui@sfu.ca>
2023-09-17 12:47:57 -0700

    Project Description Page Done

--- src/components/Projects/ProjectDescription.tsx
+++ src/components/Projects/ProjectDescription.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Text, Container, Grid, Skeleton } from '@mantine/core'
+import { createStyles, Text, Container, Grid, Skeleton, rem } from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+    grid: {
+        borderRight: `${rem(2)} solid ${theme.colors.gray[2]}`,
+    }
+  }));
 
 export function ProjectDescription() {
+    const { classes, theme } = useStyles();
+
     return (
         <>
             <Container>
                 <Grid columns={3}>
-                    <Grid.Col span={2}>
+                    <Grid.Col className={classes.grid} span={2}>
                         <Text>Typing in Skeleton</Text>
                     </Grid.Col>
                     <Grid.Col span={1}>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+import {  Title, Container} from '@mantine/core'
 import { ImageCarousel, ProjectDescription } from '../components/index';
 
 export const Projects = () => {
   return (
     <>
-      <ImageCarousel />
-      <ProjectDescription />
+      <Container>
+        <Title align="left">Project Title</Title>
+        <ImageCarousel />
+        <ProjectDescription />
+      </Container>
     </>
   );
 };
