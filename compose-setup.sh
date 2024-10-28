9bcd84701a53392f57a7b91cb90d271b569b9888
Francis C <fchui@sfu.ca>
2024-10-28 13:28:54 -0700

    Added database password option

--- compose-setup.sh
+++ compose-setup.sh
@@ -1,33 +1,54 @@
 
-echo $'Change current domain name value? y or n?' 
-awk -F '[$()]+' '/DOMAIN/{print $1$2}' .env;  source .env; 
-while true; do
-    read -p $'' yn
-    case $yn in
-        [Yy]* ) read -p "Write in new domain name:" domain; sed -i~ "/^DOMAIN_NAME=/s/=.*/=${domain}/" .env; 
-            echo "New domain changed to: ${domain}"; break;;
-        [Nn]* ) break;; 
-        * ) echo "Please answer yes or no";;
+PS3="Please select an option => "
+
+select OPTION in compose-setup database-password;
+do
+    case $OPTION in 
+        compose-setup)
+            echo $'Change current domain name value? y or n?';
+            awk -F '[$()]+' '/DOMAIN/{print $1$2}' .env;  source .env; 
+            while true; do
+                read -p $'' yn;
+                case $yn in
+                    [Yy]* ) read -p "Write in new domain name:" domain; sed -i~ "/^DOMAIN_NAME=/s/=.*/=${domain}/" .env; 
+                        echo "New domain changed to: ${domain}"; break;;
+                    [Nn]* ) break;; 
+                    * ) echo "Please answer yes or no";;
+                esac
+            done
+
+            while true; do
+                read -p $'Run nginx in staging mode or production mode? s=staging, p=production \n' sp;
+                case $sp in
+                    [Ss]* ) cp ./nginx-conf/nginx.staging.conf ./nginx-conf/nginx.conf; echo -e "Staging config loaded"; break;;
+                    [Pp]* ) cp ./nginx-conf/nginx.prod.conf ./nginx-conf/nginx.conf; echo -e "Production config loaded"; break;;
+                    * ) echo "Please answer staging or production.";;
+                esac
+            done
+
+            while true; do
+                read -p $'Would you like a staging certificate, a production certificate (rate limited),or reinstall an existing one? 
+                        s=staging, p=production, r=reinstall\n' certificate;
+                case $certificate in
+                    [Ss]* ) sed -i~ '/^CERTBOT_OPTION=/s/=.*/=staging/' .env; echo -e "Staging Certification config loaded"; break;;
+                    [Pp]* ) sed -i~ '/^CERTBOT_OPTION=/s/=.*/=force-renewal/' .env; echo -e "Production Certification config loaded"; break;;
+                    [Rr]* ) sed -i~ '/^CERTBOT_OPTION=/s/=.*/=reinstall/' .env; echo -e "Reinstall Certification config loaded"; break;;
+                    * ) echo "Please answer staging, production, reinstall.";;
+                esac
+            done
+            break;;
+        database-password)
+            echo "Change the database password? y or n?";
+            while true; do
+                read -p $'' yn;
+                case $yn in
+                    [Yy]* ) read -s -p "New DB password:" dbPass; 
+                        echo "${dbPass}" > ./db/password.txt; 
+                        echo "New database password changed in db/password.txt"; break;;
+                    [Nn]* ) break;; 
+                    * ) echo "Please answer yes or no";;
+                esac
+            done
+            break;;
     esac
 done
-
-while true; do
-    read -p $'Run nginx in staging mode or production mode? s=staging, p=production \n' sp
-    case $sp in
-        [Ss]* ) cp ./nginx-conf/nginx.staging.conf ./nginx-conf/nginx.conf; echo -e "Staging config loaded"; break;;
-        [Pp]* ) cp ./nginx-conf/nginx.prod.conf ./nginx-conf/nginx.conf; echo -e "Production config loaded"; break;;
-        * ) echo "Please answer staging or production.";;
-    esac
-done
-
-while true; do
-    read -p $'Would you like a staging certificate, a production certificate (rate limited),or reinstall an existing one? 
-            s=staging, p=production, r=reinstall\n' certificate
-    case $certificate in
-        [Ss]* ) sed -i~ '/^CERTBOT_OPTION=/s/=.*/=staging/' .env; echo -e "Staging Certification config loaded"; break;;
-        [Pp]* ) sed -i~ '/^CERTBOT_OPTION=/s/=.*/=force-renewal/' .env; echo -e "Production Certification config loaded"; break;;
-        [Rr]* ) sed -i~ '/^CERTBOT_OPTION=/s/=.*/=reinstall/' .env; echo -e "Reinstall Certification config loaded"; break;;
-        * ) echo "Please answer staging, production, reinstall.";;
-    esac
-done
-
